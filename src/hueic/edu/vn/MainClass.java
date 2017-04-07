6355920844e304590ce1f1104cbc6f722fe3ac9c
QuocTuan1998 <quoctuanb6@gmail.com>
2017-04-07 18:11:13 +0700

    4/7/2017

---
+++ src/hueic/edu/vn/MainClass.java
@@ -0,0 +1,99 @@
+package hueic.edu.vn;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// int a =1;
+		// int b=2;
+		// System.out.println(a+b);
+
+		// int a=1;
+		// if (a%2==0) {
+		// System.out.println("là số chẵn");
+		// } else {
+		// System.out.println("là số lẻ");
+		// }
+		//
+		// cho một ký tự m có thể nhận một trong 4 giá trị x h t d in xuan ha
+		// thu dong con k in ra không phải mùa trong năm,
+
+		// char m = 'g';
+		// switch (m) {
+		// case 'x':
+		// System.out.println("mùa xuân");
+		// break;
+		// case 'h':
+		// System.out.println("mùa hạ");
+		// break;
+		// case 't':
+		// System.out.println("mùa thu");
+		// break;
+		// case 'd':
+		// System.out.println("mùa đông");
+		// break;
+		// default:
+		// System.out.println("không phải mùa trong năm");
+		// break;
+		// }
+		// int s=0;
+		// for (int i = 1; i <= 10; i++) {
+		// s=s+i;
+		// }
+		// System.out.println(s);
+
+//		int[] a = new int[3];
+//		a[0] = 1;
+//		a[1] = 2;
+//		a[2] = 3;
+//		// a[3]=4;
+//		int s = 0;
+//		for (int x : a) {
+//			s = s + x;
+//		}
+//		System.out.println(s);
+	
+		
+//		int s=0;
+//		int x=1;
+//		while(x<=10){
+//			s=s+x;//cách khác s=s+x++;
+//			x++;
+//		}
+//		System.out.println(s);
+	
+//		do {
+//			s=s+x++;
+//		} while (x<=10);
+//		System.out.println(s);
+	
+//		float a=4.0f;
+//		float b=6.0f;
+//		System.out.println(TongSoThuc(a, b));
+//		String s=new String("Hello World");
+		
+	}
+	
+//	public static float TongSoThuc(float a,float b){
+//		
+//		float s;
+//		s=a+b;
+//		return s;
+//
+
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
